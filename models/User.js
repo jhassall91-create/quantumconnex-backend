0939943fa66faf49df5f5ba7fc7052ef4152e531
Jess <phoenixascensionmethod@gmail.com>
2026-05-26 12:19:38 +1000

    add authentication system

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: { type: String, unique: true },
+  password: String,
   createdAt: {
     type: Date,
     default: Date.now
